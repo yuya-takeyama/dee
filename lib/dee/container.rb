2558b3d7dc6b441558f142ada102ededfd5458dd
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-12-08 22:26:51 +0900

    Implement SingletonFactory

--- lib/dee/container.rb
+++ lib/dee/container.rb
@@ -1,4 +1,36 @@
 module Dee
   class Container
+    class Factory
+    end
+
+    class SingletonFactory < Factory
+      def initialize(&block)
+        @block = block
+        @value = nil
+      end
+
+      def call
+        @value = @block.call unless @value
+        @value
+      end
+    end
+
+    def initialize
+      @values = Hash.new
+    end
+
+    def singleton(key, &block)
+      @values[key] = SingletonFactory.new(&block)
+    end
+
+    def [](key)
+      raise "'%s' is not defined in container" unless @values.key? key
+
+      value = @values[key]
+
+      if value.kind_of? Factory
+        value.call
+      end
+    end
   end
 end
--- spec/dee/container_spec.rb
+++ spec/dee/container_spec.rb
@@ -1,2 +1,26 @@
 describe Dee::Container do
+  let(:container) { Dee::Container.new }
+
+  describe '#[]' do
+    it 'raises error when no defined key is specified' do
+      expect { container['no_defined_key'] }.to raise_error
+    end
+  end
+
+  describe '#singleton' do
+    before do
+      container.singleton 'hash' do
+        Hash.new
+      end
+    end
+
+    it 'creates object defined in factory' do
+      expect(container['hash']).to eq(Hash.new)
+    end
+
+    it 'always returns same object' do
+      a = container['hash']
+      expect(container['hash']).to equal(a)
+    end
+  end
 end
